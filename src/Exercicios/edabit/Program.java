9bb9c7269393fd858665ed9f83298cf4c10f43e8
genildocs <genildocs@gmail.com>
2023-08-23 08:09:56 -0300

    commit

--- src/Exercicios/edabit/Program.java
+++ src/Exercicios/edabit/Program.java
@@ -14,11 +14,7 @@ public class Program {
 
     public static boolean isSameNum(int x, int y) {
 
-        if(x == y){
-            return true;
-        }else{
-            return false;
-        }
+        return x == y;
 
     }
 
